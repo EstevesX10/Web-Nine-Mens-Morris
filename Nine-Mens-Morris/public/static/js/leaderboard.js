3ebdc8174a876d66e83e07a1a2fc7e0a3db65c80
PsuperX <ja435364@gmail.com>
2024-11-04 16:03:52 +0000

    leaderboard not working yet

--- Nine-Mens-Morris/public/static/js/leaderboard.js
+++ Nine-Mens-Morris/public/static/js/leaderboard.js
@@ -1,1 +1,24 @@
-// CODE HERE
+class Leaderboard {
+  Leaderboard() {
+    // Keys are "difficulty_size"
+    // Values are list [wins, losses]
+    this.singleplayer = {};
+
+    // Keys are player names
+    // Values are scores
+    this.multiplayer = {};
+  }
+
+  update_singleplayer(winner, difficulty, size) {
+    this.singleplayer[`${difficulty}_${size}`][winner - 1]++;
+
+    cell = document.query(".leaderboard-singleplayer");
+    cell.textContent = this.singleplayer[`${difficulty}_${size}`][winner - 1];
+  }
+
+  generate_row() {
+    // TODO: this
+  }
+}
+
+g_leaderboard = new Leaderboard();
