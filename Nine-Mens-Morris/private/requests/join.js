36b6040239b7527a01d61862d407c994f46c3e59
Gongas <goncalo.mesteves03@gmail.com>
2024-12-04 23:29:21 +0000

    Implemented the join request - May need some further tunning with the update request

---
+++ Nine-Mens-Morris/private/requests/join.js
@@ -0,0 +1,133 @@
+import {
+  receive,
+  send,
+  error,
+  readUsers,
+  userExists,
+  getUser,
+  addUser,
+  saveUsers,
+} from "./utils.js";
+import crypto from "crypto";
+
+/* 
+[NOTE - Sessions Structuring ] 
+sessions = { 
+    hash: {         <game Hash>
+        ongoing:    <true || false>
+        finished:   <true || false>
+        size:       <2 || 3 || 4>
+        player1:    <player 1 Nick>
+        player2:    <player 2 Nick>
+    } 
+}
+*/
+
+let sessions = {};
+let finishedSessions = {};
+
+function lookUpSessions(selectedSize) {
+  // Fetch available sessions
+  let openSessions = Object.entries(sessions)
+    .filter(
+      ([hash, details]) =>
+        details.size === selectedSize && !details.ongoing && !details.finished
+    )
+    .map(([hash]) => hash);
+
+  // Return the joinable sessions
+  return openSessions;
+}
+
+function generateGameHash(username, boardSize) {
+  // Get the plaintext to use for the hash (Uses the username, board size and current date)
+  const plainText = username + boardSize + Date.now().toString();
+  // Compute and return the game hash
+  return crypto.createHash("sha256").update(plainText).digest("hex");
+}
+
+function addNewSession(username, selectedSize) {
+  // Generate a new game hash
+  let newGameHash = generateGameHash(username, selectedSize);
+
+  // Define a new session
+  let newSession = {
+    ongoing: false,
+    finished: false,
+    size: selectedSize,
+    player1: username,
+    player2: undefined,
+  };
+
+  // Add the new session
+  sessions[newGameHash] = newSession;
+
+  // return game hash
+  return newGameHash;
+}
+
+function joinExistingSession(sessionGameHash, username) {
+  // Update the existing session
+  if (sessions[sessionGameHash]) {
+    sessions[sessionGameHash].ongoing = true;
+    sessions[sessionGameHash].player2 = username;
+  }
+  // Return the session game hash
+  return sessionGameHash;
+}
+
+export const join = async (req, res) => {
+  // Get the request
+  let userJoin = await receive(req);
+
+  // Check if any argument was ommised
+  if (
+    !userJoin.nick ||
+    !userJoin.password ||
+    !userJoin.group ||
+    !userJoin.size
+  ) {
+    // Missing Arguments
+    return error(
+      res,
+      "Missing Arguments! Please ensure your request contains all the information!"
+    );
+  }
+
+  // Verify the integrity of Board Size
+  if (userJoin.size < 2 && userJoin.size > 4) {
+    // Invalid Board Size
+    return error(
+      res,
+      `Invalid Board Size (${userJoin.size})! Please choose between [2, 3, 4] !`
+    );
+  }
+
+  // Check if the received nick exists
+  if (!userExists(userJoin.nick)) {
+    // Inexistent User
+    return error(
+      res,
+      `Invalid User (${userJoin.nick})! Please perform Registration!`
+    );
+  }
+
+  // Create variables for the available sessions and the current game hash
+  let availableSessions, gameHash;
+
+  // Look Up Available Sessions
+  availableSessions = lookUpSessions(userJoin.size);
+
+  // Check if there are any available sessions
+  if (availableSessions.length > 0) {
+    gameHash = joinExistingSession(availableSessions[0], userJoin.nick);
+  } else {
+    // Create a new session
+    gameHash = addNewSession(userJoin.nick, userJoin.size);
+  }
+
+  console.log(sessions);
+
+  // Send the final message with the game hash
+  return send(res, { game: gameHash });
+};
